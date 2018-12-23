b2eb30cf10c525f60615cd8f884518acba5167f2
r-hvid <lcb852@alumni.ku.dk>
2018-12-23 17:11:47 +0100

    Added boilerplate to git

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+import Layout from './Layout.js';
+
+class Landing extends React.Component {
+    render() {
+        return (
+            <div class="jumbotron">
+                <h1 class="display-4">Hello, world!</h1>
+                <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <hr class="my-4"/>
+                <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+                <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+            </div>
+        );
+    }
+}
+
+
+class App extends React.Component {
+    render() {
+        return (
+            <Landing/>
+        );
+    }
+}
+
+export default App;
---
+++ src/Layout.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class Layout extends React.Component {
+    render() {
+        <div className="wrapper">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand" href="#">Navbar w/ text</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarText">
+                    <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Pricing</a>
+                    </li>
+                    </ul>
+                    <span class="navbar-text">
+                    Navbar text with an inline element
+                    </span>
+                </div>
+            </nav>
+            <div className="container">
+                {this.props.children}
+            </div>
+        </div>
+    }
+}
+
+export default Layout;
